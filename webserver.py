15793a81c741020165af307aab62af40b9c1b5dd
unknown <rcbuelvasv@gmail.com>
2018-11-26 10:04:45 -0500

    Docker exploded

--- webserver.py
+++ webserver.py
@@ -4,14 +4,15 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-app.config['MONGO_DBNAME'] = 'author_db'
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/author_db'
+app.config['MONGO_DBNAME'] = 'authors_db'
+#app.config['MONGO_URI'] = 'mongodb://localhost:27018/authors_db'
+app.config['MONGO_URI'] = 'mongodb://192.168.99.101:27018/authors_db'
 
 mongo = PyMongo(app)
 
 @app.route('/author/search/all', methods=['GET'])
 def get_all_authors():
-    db = mongo.db.author_db
+    db = mongo.db.authors_db
     output = []
     for q in db.find():
         output.append({'username' : q['username'], 'name' : q['name'], 'projects' : q['projects']})
@@ -19,7 +20,7 @@ def get_all_authors():
 
 @app.route('/author/<username>', methods=['GET'])
 def get_author(username):
-    db = mongo.db.author_db
+    db = mongo.db.authors_db
     q = db.find_one({'username' : username})
     if q:
         output = {'username' : q['username'], 'name' : q['name'], 'projects' : q['projects']}
@@ -29,7 +30,7 @@ def get_author(username):
 
 @app.route('/author/create', methods=['POST'])
 def create_author():
-    db = mongo.db.author_db 
+    db = mongo.db.authors_db 
     username = request.json['username']
     name = request.json['name']
     projects = request.json['projects']
@@ -44,20 +45,20 @@ def create_author():
 
 @app.route('/author/<username>', methods=['DELETE'])
 def delete_author(username):
-    db = mongo.db.author_db
+    db = mongo.db.authors_db
     output = db.delete_one({'username' : username}).acknowledged
     return jsonify({'result' : output})
     
 @app.route('/author/<username>/addProject', methods=['PATCH'])
 def add_project(username):
-    db = mongo.db.author_db
+    db = mongo.db.authors_db
     project = request.json['project']
     output = db.update_one({'username': username}, {'$push': {'projects': project}}).acknowledged
     return jsonify({'result' : output})
 
 @app.route('/author/<username>/removeProject', methods=['PATCH'])
 def add_project(username):
-    db = mongo.db.author_db
+    db = mongo.db.authors_db
     project = request.json['project']
     output = db.update_one({'username': username}, {'$pull': {'projects': project}}).acknowledged
     return jsonify({'result' : output})
