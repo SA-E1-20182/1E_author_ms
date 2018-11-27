a913ab47bd0bc20755ed41327495d09cac83f7d3
unknown <rcbuelvasv@gmail.com>
2018-11-26 22:44:09 -0500

    Mongo

--- webserver.py
+++ webserver.py
@@ -4,8 +4,8 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 app.config['MONGO_DBNAME'] = 'authors_db'
-#app.config['MONGO_URI'] = 'mongodb://localhost:27018/authors_db'
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/authors_db'
+app.config['MONGO_URI'] = 'mongodb://localhost:27018/authors_db'
+#app.config['MONGO_URI'] = 'mongodb://192.168.99.101:27018/authors_db'
 
 mongo = PyMongo(app)
 db = mongo.db.authors_db
@@ -58,7 +58,7 @@ def remove_project(username):
     return jsonify({'result' : output})
 
 
-    
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7999)
     app.run(debug=True)
